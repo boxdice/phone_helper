5ded4a0318f469b3fdb8c909cb536c962c507e66
Vladimir Bobes Tuzinsky <vladimir@tuzinsky.com>
2016-05-24 22:52:08 +0200

    Added tests for CountryList

---
+++ test/country_list_test.rb
@@ -0,0 +1,36 @@
+require "test_helper"
+
+class CountryListTest < Minitest::Test
+
+  def test_loading_of_countries
+    countries = PhoneHelper::CountryList.all
+    assert_kind_of Array, countries
+    country_codes = countries.map(&:alpha2)
+
+    missing = Phonelib.phone_data.keys - country_codes
+    assert_equal ["001"], missing # 001 - International Premium Rate Service
+
+    obsolete = country_codes - Phonelib.phone_data.keys
+    assert_equal [], obsolete
+  end
+
+  def test_finding_by_alpha2
+    PhoneHelper::CountryList.all.each do |country|
+      assert_equal country, PhoneHelper::CountryList[country.alpha2]
+    end
+  end
+
+  def test_finding_by_alpha3
+    PhoneHelper::CountryList.all.each do |country|
+      next unless country.alpha3
+      assert_equal country, PhoneHelper::CountryList[country.alpha3]
+    end
+  end
+
+  def test_finding_by_name
+    PhoneHelper::CountryList.all.each do |country|
+      assert_equal country, PhoneHelper::CountryList[country.name]
+    end
+  end
+
+end
