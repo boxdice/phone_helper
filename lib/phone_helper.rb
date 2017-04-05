f686379bf95c82a98ed5d8a0abe2fc1e206e014c
Vladimir Bobes Tuzinsky <vladimir@tuzinsky.com>
2017-04-05 16:21:30 +0200

    Added new argument to PhoneHelper module methods

--- lib/phone_helper.rb
+++ lib/phone_helper.rb
@@ -7,20 +7,20 @@ module PhoneHelper
 
   module_function
 
-  def plausible?(value, country: nil, calling_code: nil)
-    Number.new(value, country: country, calling_code: calling_code).plausible?
+  def plausible?(value, country: nil, calling_code: nil, postcode: nil)
+    Number.new(value, country: country, calling_code: calling_code, postcode: postcode).plausible?
   end
 
-  def normalize(value, country: nil, calling_code: nil)
-    Number.new(value, country: country, calling_code: calling_code).normalized
+  def normalize(value, country: nil, calling_code: nil, postcode: nil)
+    Number.new(value, country: country, calling_code: calling_code, postcode: postcode).normalized
   end
 
-  def format(value, country: nil, calling_code: nil)
-    Number.new(value, country: country, calling_code: calling_code).formatted
+  def format(value, country: nil, calling_code: nil, postcode: nil)
+    Number.new(value, country: country, calling_code: calling_code, postcode: postcode).formatted
   end
 
-  def search_index(value, country: nil, calling_code: nil)
-    Number.new(value, country: country, calling_code: calling_code).search_index
+  def search_index(value, country: nil, calling_code: nil, postcode: nil)
+    Number.new(value, country: country, calling_code: calling_code, postcode: postcode).search_index
   end
 
 end
